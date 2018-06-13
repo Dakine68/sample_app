7e5b7d517cdd8d8751a48568b452af99349fc007
Gallyamov Aydar <gallyamov.aydar@gmail.com>
2018-06-13 13:58:50 +0500

    Finish static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/...', to: 'static_pages#home'
-
   get 'static_pages/home', to: 'static_pages#home'
-
   get 'static_pages/help', to: 'static_pages#home'
-
   get 'static_pages/about', to: 'static_pages#home'  
 
-  root 'static_pages#home'
+  
 end
